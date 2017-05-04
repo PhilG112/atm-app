6d005b68e64bdf23a6d312ba911ef84c2777c73a
Philg112 <phil.grujovski@gmail.com>
2017-05-04 12:57:31 +1000

    Backend functionality finished. Added styles

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -12,19 +12,24 @@ class TransactionsController < ApplicationController
   end
 
   def new
-    # TODO: Ask jack how to prevent closed accoutns form showing up in the collection_select
     @transaction = Transaction.new
   end
 
   def create
-    # TODO: Check if from_account has enough money to perform the transaction
     from_account = params["transaction"]["from_account_id"].to_i
     to_account = params["transaction"]["to_account_id"].to_i
     amount = params["transaction"]["transfer_amount"].to_f
-    Transaction.credit_account(from_account, amount)
-    Transaction.debit_account(to_account, amount)
-    flash[:transaction_outcome] = "Transaction Complete."
-    redirect_to(customer_path(@current_user))
+
+    if(check_account_balance(from_account, amount))
+      Transaction.credit_account(from_account, amount)
+      Transaction.debit_account(to_account, amount)
+      flash[:successful_transaction] = "Transaction Complete."
+      redirect_to(customer_path(@current_user))
+    else
+      flash[:failed_transaction] = "From account has insufficent funds."
+      @transaction = Transaction.new
+      render :new
+    end
   end
 
   private
@@ -33,7 +38,10 @@ class TransactionsController < ApplicationController
       flash[:error] = "You need to login to access that page."
       redirect_to(login_path())  
     end
-    
   end
-  
+
+  def check_account_balance(acc, amount)
+    a = Account.find_by(id: acc)
+    a.account_balance > amount
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.time_zone = 'Sydney'
+  config.active_record.default_timezone = :local
   # Code is not reloaded between requests.
   config.cache_classes = true
 
